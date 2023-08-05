90247a5737b480f8928b85ff846cba8a00844d7c
karthikpeddi14 <128394123+karthikpeddi14@users.noreply.github.com>
2023-08-05 11:44:44 +0530

    Update Buy and Sell Stock - II.cpp

--- Buy and Sell Stock - II.cpp
+++ Buy and Sell Stock - II.cpp
@@ -71,3 +71,29 @@ public:
     }
 };
 // tabulation
+
+ // space opti
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) 
+    {
+        int n=prices.size();
+        vector<int> next(2,0),cur(2,0);
+        
+        for(int i=n-1; i>=0 ; i--)
+        {
+            int profit = 0;
+            for(int j=0 ; j<2; j++)
+            {
+                if(j)
+                profit = max(-prices[i]+next[0],next[1]); 
+                else
+                profit = max(prices[i]+next[1],next[0]);
+                cur[j] = profit;
+            }
+            next = cur;
+        }
+        return next[1];
+    }
+};
+// space opti;
